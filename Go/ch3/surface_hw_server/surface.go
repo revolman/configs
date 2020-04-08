f8b3415fa8c0298a1cba3563b30b55155d5b7a66
Ivn Revolman <revolman.eis@gmail.com>
2020-04-08 22:55:49 +0300

    ch 3.5.4

--- Go/ch3/surface_hw_server/surface.go
+++ Go/ch3/surface_hw_server/surface.go
@@ -10,15 +10,15 @@ import (
 const (
 	width, height = 600, 320
 	cells         = 100
-	xyrange       = 30.0
-	xyscale       = width / 2 / xyrange
-	zscale        = height * 0.4
-	angle         = math.Pi / 6
+	xyrange = 30.0
+	xyscale = width / 2 / xyrange
+	zscale  = height * 0.4
+	angle   = math.Pi / 6
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
-func surface(out io.Writer, cells int) {
+func surface(out io.Writer) {
 	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke:grey; stroke-width: 0.7' "+
 		"width='%d' height='%d'>\n", width, height)
